a3cb2f49093a82d2a81a1277aaeb5b3106596c39
Shaun Lindsley <shaunlindsley@gmail.com>
2021-10-26 08:15:52 +0100

    Added rest of fields to the Lead model

--- leads/models.py
+++ leads/models.py
@@ -2,9 +2,22 @@ from django.db import models
 
 
 class Lead(models.Model):
+
+    SOURCE_CHOICES = (
+        ('YouTube', 'YouTube'),
+        ('Google', 'Google'),
+        ('Newsletter', 'Newsletter'),
+    )
+
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
+
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(choices=SOURCE_CHOICES, max_length=200)
+
+    profile_picture = models.ImageField(blank=True, null=True)
+    special_files = models.FileField(blank=True, null=True)
 
     def __str__(self):
         return self.first_name
