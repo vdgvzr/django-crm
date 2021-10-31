abf70b993a84f18fe13a72b51cda7ae08d73a7b5
Shaun Lindsley <shaunlindsley@gmail.com>
2021-10-31 17:16:24 +0000

    Added str method to leads.models

--- leads/models.py
+++ leads/models.py
@@ -8,8 +8,11 @@ class User(AbstractUser):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+    """ first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200) """
+
+    def __str__(self):
+        return self.user.email
 
 
 class Lead(models.Model):
@@ -17,6 +20,9 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
     """ SOURCE_CHOICES = (
         ('YouTube', 'YouTube'),
