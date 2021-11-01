f793d4f6f7ba919a522546fc9f0b69cb8e43f962
Shaun Lindsley <shaunlindsley@gmail.com>
2021-11-01 21:29:24 +0000

    Added home page template and settings

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+
+    template = "leads/home_page.html"
+    context = {}
+
+    return render(request, template, context)
