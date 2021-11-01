eb18f9145240ba092da7dbeaf34d8d48b6ab67af
Shaun Lindsley <shaunlindsley@gmail.com>
2021-11-01 22:06:51 +0000

    Listing out leads with basic styling on leads page

--- leads/views.py
+++ leads/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Lead
 
 
-def home_page(request):
-
-    template = "leads/home_page.html"
-    context = {}
+def lead_list(request):
+    template = "leads/lead_list.html"
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads,
+    }
 
     return render(request, template, context)
