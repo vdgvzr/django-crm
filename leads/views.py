290d8048866713f922c7d8863cb9a2a73ee99930
Shaun Lindsley <shaunlindsley@gmail.com>
2021-12-28 15:01:07 +0000

    Completed lead update function with model form

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Lead, Agent
+from .models import Lead
 from .forms import LeadForm, LeadModelForm
 
 
@@ -23,7 +23,9 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
+    template = "leads/lead_create.html"
     form = LeadModelForm()
+
     if request.method == 'POST':
         form = LeadModelForm(request.POST)
         if form.is_valid():
@@ -31,11 +33,53 @@ def lead_create(request):
 
             return redirect("/leads")
 
-    template= "leads/lead_create.html"
     context = {
         "form": form
     }
     return render(request, template, context)
+
+
+def lead_update(request, pk):
+    template = "leads/lead_update.html"
+    lead = Lead.objects.get(pk=pk)
+    form = LeadModelForm(instance=lead)
+
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+
+            return redirect("/leads")
+
+    context = {
+        "lead": lead,
+        "form": form
+    }
+    return render(request, template, context)
+
+
+''' def lead_update(request, pk):
+    template = "leads/lead_update.html"
+    lead = Lead.objects.get(pk=pk)
+    form = LeadForm()
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            lead.first_name = first_name
+            lead.last_name = last_name
+            lead.age = age
+
+            lead.save()
+
+            return redirect("/leads")
+    context = {
+        "lead": lead,
+        "form": form
+    }
+    return render(request, template, context) '''
 
 
 ''' def lead_create(request):
