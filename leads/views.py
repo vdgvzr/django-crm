8ed748c4ed92cc27e8608e04cb7cff11f1b610f2
Shaun Lindsley <shaunlindsley@gmail.com>
2021-12-29 15:46:26 +0000

    Added authentication functionality

--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import reverse
 from django.core.mail import send_mail
 from .models import Lead
-from .forms import LeadModelForm
+from .forms import LeadModelForm, CustomUserCreationForm
 from django.views import generic
+
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("login")
+
 
 
 class LandingPageView(generic.TemplateView):
