d6fdf16ff5342a11924912a10ff7814501d0cb1d
Shaun Lindsley <shaunlindsley@gmail.com>
2021-12-30 17:31:01 +0000

    Added agent delete functionality

--- agents/urls.py
+++ agents/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import AgentListView, AgentCreateView, AgentDetailView, AgentUpdateView
-
+from .views import (
+    AgentListView,
+    AgentCreateView,
+    AgentDetailView,
+    AgentUpdateView,
+    AgentDeletelView,
+)
 
 app_name = 'agents'
 
@@ -9,5 +14,6 @@ urlpatterns = [
     path('', AgentListView.as_view(), name='agent-list'),
     path('<int:pk>/', AgentDetailView.as_view(), name='agent-detail'),
     path('<int:pk>/update/', AgentUpdateView.as_view(), name='agent-update'),
+    path('<int:pk>/delete/', AgentDeletelView.as_view(), name='agent-delete'),
     path('create/', AgentCreateView.as_view(), name='agent-create'),
 ]
--- agents/views.py
+++ agents/views.py
@@ -44,3 +44,14 @@ class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
 
     def get_success_url(self):
         return reverse("agents:agent-list")
+
+
+class AgentDeletelView(LoginRequiredMixin, generic.DeleteView):
+    template_name = "agents/agent_delete.html"
+    context_object_name = "agent"
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
+    def get_queryset(self):
+        return Agent.objects.all()
